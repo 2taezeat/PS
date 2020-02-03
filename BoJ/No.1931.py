c0a594e2ef19dcdaecbe16b51602997c29912197
2taezeat <xorudfl772@naver.com>
2020-02-03 18:53:57 +0900

    200203 // No.1931.py // Greedy

---
+++ BoJ/No.1931.py
@@ -0,0 +1,19 @@
+import sys
+N = int(sys.stdin.readline())
+array1 = [ [0, 0] ]
+
+for i in range(N):
+    s,f = map(int,sys.stdin.readline().split())
+    array1.append([f,s])
+
+list.sort(array1, key = lambda x : x[1])
+shellSort(array1)
+
+result = 1
+k = 1
+for m in range(2,N+1):
+    if array1[m][1] >= array1[k][0]:
+        result = result + 1
+        k = m
+
+print(result)
