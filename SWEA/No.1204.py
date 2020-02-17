b0be02242954634f39fb175d664ef1c28245c4ab
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-02-17 18:01:54 +0900

    200217 // SWEA.1204 // 최빈수

---
+++ SWEA/No.1204.py
@@ -0,0 +1,18 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    l1 = list(map(int, input().split()))
+    l1.sort()
+    countlist = []
+    der_l1 = list(set(l1))
+
+    for el in der_l1:
+        count = 0 
+        for j in range(len(l1)):
+            if el == l1[j]:
+                count = count + 1
+
+        countlist.append([count,el])
+    #print(countlist)
+    r = countlist.index(max(countlist))
+    print('#',N,' ',der_l1[r], sep="")
