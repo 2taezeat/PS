c2ab9c1a812fb7b4f9b9be147e4ce1ae00d2557c
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-02-17 16:42:31 +0900

    200217 // No.1254 // DP

--- BoJ/No.1254.py
+++ BoJ/No.1254.py
@@ -1,1 +1,21 @@
-S = input()
+S = list(input())
+Slen = len(S)
+result = 0
+for i in range(Slen+1):
+    ss = []
+    ss.extend(S)
+    c = 0
+    for j in range(i-1,-1,-1):
+        ss.append(S[j])
+
+    sslen = len(ss)  
+    for k in range(sslen // 2): # 팰린드롬 확인
+        if ss[k] != ss[-k-1]:
+            c = -1
+            break
+
+    if c == 0:
+        result = sslen
+        break
+    
+print(result)
