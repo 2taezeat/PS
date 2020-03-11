73350711d4fc3eb2f25ceebfd9f4e13d459990eb
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-03-11 16:32:36 +0900

    200311 / 1

---
+++ BoJ/BoJ.1339.py
@@ -0,0 +1,24 @@
+N = int(input())
+d = [[],[],[],[],[],[],[],[],[],[]]
+alp = [0] * 26
+for i in range(N):
+    l1 = input()
+    length = len(l1)
+
+    for j in range(length-1,-1,-1):
+        d[j].insert(j,l1[length - j- 1])
+
+i = 1
+for a in d:
+    for ch in a:
+        alp[ord(ch)-65] = alp[ord(ch)-65] + (1*i)
+    i = i * 10
+alp.sort(reverse=True)
+
+r = 9
+sum1 = 0
+for i in alp:
+    sum1 = sum1 + i*r
+    r = r - 1
+
+print(sum1)
