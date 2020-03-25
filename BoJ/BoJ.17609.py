068a9d4f4e2c7bf3003e5fe94b9014c5f6ad1f38
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-03-25 14:07:07 +0900

    200325 / 1 / BoJ.17609.py (Success)

--- BoJ/BoJ.17609.py
+++ BoJ/BoJ.17609.py
@@ -10,10 +10,28 @@ for i in range(T):
             p = 1
             break
 
+    if p == 0:
+        print(p)
+    else:
+        list1 = list(s)
+        list2 = list(s)
+        list1.pop(c)
+        list2.pop(l-c-1)
+        l1 = len(list1)
+        
+        p1,p2 = 1,1
+        for c in range(0,l1//2):
+            if list1[c] != list1[l1-c-1]:
+                p1 = 2
+                break
+        
 
-    print('c',c)
+        for c in range(0,l1//2):
+            if list2[c] != list2[l1-c-1]:
+                p2 = 2
+                break
 
-    s1= s.pop(c)
-
-    print(s1)
-############# 미 성공
+        if p1 == 1 or p2 == 1:
+            print(1)
+        else:
+            print(2)
