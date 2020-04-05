88df1484c43d3e296cf4d8ef0e131ba2c4c739fa
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-04-05 19:59:52 +0900

    200405 / 1 / BoJ.1065 / s

--- BoJ/BoJ.1065.py
+++ BoJ/BoJ.1065.py
@@ -1,1 +1,20 @@
-#### 200326 
+N = int(input())
+c = 0
+for i in range(1,N+1):
+    p = 0
+    if 0 < i < 10:
+        c += 1
+        continue
+    else:
+        i = list(str(i))
+        d = int(i[1]) - int(i[0])
+
+        for n in range(1,len(i)):
+            if d != int(i[n]) - int(i[n-1]):
+                p = 1
+                break
+
+    if p == 0:
+        c += 1
+
+print(c)
