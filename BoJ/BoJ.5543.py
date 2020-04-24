60012cafead1161b81328eba909e1a6a3f3344f9
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-04-24 10:51:20 +0900

    200424 / 1

---
+++ BoJ/BoJ.5543.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+l1,l2 = [],[]
+l1.append(a)
+l1.append(b)
+l1.append(c)
+
+l2.append(d)
+l2.append(e)
+
+
+print(min(l1)+min(l2)-50)
+
