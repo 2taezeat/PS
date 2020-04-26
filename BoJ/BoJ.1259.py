403808e085f9eac11fefda6dcd9074e2c5ac8bd4
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-04-26 11:34:51 +0900

    200426 / 1

---
+++ BoJ/BoJ.1259.py
@@ -0,0 +1,16 @@
+while(True):
+    s = input()
+    p = 0
+
+    if s == '0':
+        break
+
+    else:
+        for i in range(len(s)//2):
+            if s[i] != s[len(s)-1-i]:
+                p = 1
+
+    if p ==0:
+        print('yes')
+    elif p == 1:
+        print('no')
