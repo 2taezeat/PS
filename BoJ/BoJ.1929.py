7a3a9e4e9004b10cc4e064da974d682323a2f0d0
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-05-10 16:40:44 +0900

    200510 / 1

--- BoJ/BoJ.1929.py
+++ BoJ/BoJ.1929.py
@@ -1,10 +1,10 @@
 result = []
 M,N = map(int,input().split())
 nl = [i for i in range(2,N+1)]
-l1 = [True for i in range(N+1)]
+l1 = [True] * (N+1)
+l1[1] = l1[0] = False
 
-
-for i in range(2,M+1):
+for i in range(2,N+1):
     if l1[i] == True:
         p = i
     else:
@@ -14,14 +14,14 @@ for i in range(2,M+1):
     while(True):
         k = j * p
         j = j + 1
+         
 
         if k > N:
             break
         l1[k] = False 
 
-for i in range(M,len(l1)):
-    if i ==0 or i ==1 :
-        continue
-
+prime = []
+for i in range(M,N+1):
     if l1[i] == True:
         print(i)
+
