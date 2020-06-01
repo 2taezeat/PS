55b5ea2ad54cef9addd42c4280913b01d15ddb88
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-06-01 12:20:06 +0900

    200601 / 1

---
+++ BoJ/BoJ.11651.py
@@ -0,0 +1,11 @@
+n = int(input())
+xyl = []
+for i in range(n):
+    x,y = map(int,input().split())
+    xyl.append([x,y])
+
+
+xyl = sorted(xyl, key = lambda x : (x[1], x[0]))
+
+for i in range(n):
+    print(xyl[i][0],xyl[i][1])
