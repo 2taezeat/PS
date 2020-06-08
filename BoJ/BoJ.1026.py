e08482e010652071730952ea8e11377c98972a65
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-06-09 01:09:31 +0900

    200609 / 1

---
+++ BoJ/BoJ.1026.py
@@ -0,0 +1,11 @@
+n = int(input())
+s1 = list(map(int,input().split()))
+s2 = list(map(int,input().split()))
+
+s1.sort(reverse=True)
+s2.sort()
+sum1 = 0 
+for i in range(n):
+    sum1 = sum1 + s1[i]*s2[i]
+
+print(sum1) 
