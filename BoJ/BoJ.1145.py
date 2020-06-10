ebf101e349c3c5216d9e6bfecfd25014c4aee648
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-06-10 12:41:00 +0900

    200610 / 1

---
+++ BoJ/BoJ.1145.py
@@ -0,0 +1,15 @@
+s1 = list(map(int,input().split()))
+result = 1
+
+while(1):
+
+    count = 0
+    for i in range(5):
+        if result % s1[i] == 0:
+            count += 1
+
+    if count > 2:
+        print(result)
+        break
+
+    result += 1
