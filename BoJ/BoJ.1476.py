185ad4e5e2a0fdec96a20211563e580cb447a073
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-06-11 10:35:47 +0900

    200611 /1

---
+++ BoJ/BoJ.1476.py
@@ -0,0 +1,28 @@
+e, s, m = map(int,input().split())
+
+y = 1
+E,S,M = 1,1,1
+
+while(1):
+    if (E == e) and (S == s) and (M == m):
+        break
+    
+    E = E + 1
+    S = S + 1
+    M = M + 1
+
+    if E == 16:
+        E = 1
+
+    if S == 29:
+        S = 1
+
+    if M == 20:
+        M = 1
+
+    y = y + 1
+
+    if (E == e) and (S == s) and (M == m):
+        break
+
+print(y)
