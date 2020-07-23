599cede5dd2a8dfe5eb3d467c5477f05eadd3106
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-07-23 16:31:22 +0900

    200723 / 1 / BoJ.1655 / Success

--- BoJ/BoJ.1655.py
+++ BoJ/BoJ.1655.py
@@ -1,19 +1,24 @@
 import heapq # 최소 힙 모듈
 import sys
-import math
 input = sys.stdin.readline
 n = int(input())
-h = []
+maxh = []
+minh = []
 
 for i in range(n):
     k = int(input())
-    heapq.heappush(h,k)
 
-        
+    if len(minh) == len(maxh):
+        heapq.heappush(maxh, [-k,k] )
+    else:
+        heapq.heappush(minh, [k,k] )
 
-    k = math.log2(i+2)
-    k = math.floor(k)
 
-    j = 2**k-2
+    if (minh != []) and (maxh[0][1] > minh[0][1]):
+        t_max = heapq.heappop(maxh)[1]
+        t_min = heapq.heappop(minh)[1]
 
-    print('#',h[j])
+        heapq.heappush(maxh, [-t_min, t_min])
+        heapq.heappush(minh, [t_max, t_max])
+    
+    print(maxh[0][1])
