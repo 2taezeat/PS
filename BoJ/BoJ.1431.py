46a70229a8957e2a04998c92a84aebfd8aa88737
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-07-29 14:16:12 +0900

    200729 / 1

---
+++ BoJ/BoJ.1431.py
@@ -0,0 +1,19 @@
+t = int(input())
+l1 = []
+
+for i in range(t):
+    k = input()
+    sum1 = 0
+    for c in k:
+        try:
+            if -1 < int(c) < 10:
+                sum1 = sum1 + int(c)
+        except:
+            pass
+    
+    l1.append([k,len(k),sum1])
+
+l1 = sorted(l1, key = lambda x : (x[1], x[2], x[0]) )
+
+for i in range(t):
+    print(l1[i][0])
