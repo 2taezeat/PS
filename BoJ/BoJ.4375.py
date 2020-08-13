58d3b97a1aa9ab76ff3e969eb0307d5c5474f94d
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-08-13 21:00:39 +0900

    200813 / 1

---
+++ BoJ/BoJ.4375.py
@@ -0,0 +1,25 @@
+import sys
+l1 = []
+for i in range(1,10000):
+    b = '1'*i
+    l1.append(int(b))
+
+try:
+    while(1):
+            n = int(sys.stdin.readline())
+            a = 0
+            p = 0
+            for l in l1:
+                if l % n == 0:
+                    p = 1
+                    a = str(l)
+                    break
+
+            if p == 1:
+                print(len(a))
+
+            if p == 0:
+                continue
+
+except:
+    exit()
