477843d87fa6a38121cd2019948e32f4371a3ff4
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-15 23:43:06 +0900

    200915 / sort

---
+++ BoJ/BoJ.1715.py
@@ -0,0 +1,22 @@
+import heapq
+n = int(input())
+l1 = []
+for i in range(n):
+    heapq.heappush(l1,int(input()))
+
+r = 0
+if n == 1:
+    print(l1[0])
+elif n == 2:
+    print(l1[0]+l1[1])
+    
+else:
+    for i in range(n-1):
+        a = heapq.heappop(l1)
+        b = heapq.heappop(l1)
+        r = a + b +r
+
+        heapq.heappush(l1,a+b)
+
+    print(r)
+
