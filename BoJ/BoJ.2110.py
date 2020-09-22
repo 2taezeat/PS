3d929e123960986c0d31b9ce32c6034dd15d07eb
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-22 18:11:55 +0900

    200922 / 1 / BS

--- BoJ/BoJ.2110.py
+++ BoJ/BoJ.2110.py
@@ -10,10 +10,12 @@ end = l1[-1] - l1[0] # 집의 좌쵸중에 가장 큰 값
 result = 0
 
 while(start <= end):
+    ###### 초기화
     mid = (start + end) // 2 # mid는 가장 인접한 두 공유기 사이의 거리(gap)을 의미
     value = l1[0]
     count = 1
-
+    #######
+    
     print(start,end,mid,count)
 
     for i in range(1,n): # 앞에서 부터 차근차근 설치
