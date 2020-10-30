6465594c26d08b11502ec0df7ea4d7ef96f18103
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-10-30 13:09:37 +0900

    201030

--- BoJ/BoJ.11559.py
+++ BoJ/BoJ.11559.py
@@ -16,21 +16,21 @@ def bfs(a,b,f):
             ny = y + dy[i]
 
             if 0<= nx < 12 and 0<= ny < 6:
-                if ( space[nx][ny] == space[x][y] ) and ( visit[nx][ny] == 0 ):
+                if ( space[nx][ny] == space[x][y] ) and ( visit[nx][ny] == 0 ): # 방문한 곳이 아니고, 같은 뿌요 일 때
                     count = count + 1
                     visit[nx][ny] = 1
                     q.append( [nx,ny] )
 
-    if count > 3:
+    if count > 3: # 모인 뿌요의 갯수가 4이상이 되면
         f = f + 1
         for i in range(12):
             for j in range(6):
-                if visit[i][j] == 1:
+                if visit[i][j] == 1: # 방문한 곳을 '.' 로 바꿈.
                     space[i][j] = '.'
     
     return f
 
-
+# 뿌요의 바로 아래에 . 이 있으면 떨어져야 하므로 다른 뿌요가 있거나 바닥에 닿을 때 까지 뿌요를 떨어뜨린다.
 def fall():
     for i in range(10,-1,-1):
         for j in range(6):
@@ -47,13 +47,12 @@ def fall():
 
                 space[i][j] = '.'
 
-
 space = []
 for i in range(12):
     s = list(input())
     space.append(s)
 
-
+# 상, 하, 좌, 우
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 answer = 0
@@ -65,10 +64,10 @@ while True:
             if space[i][j] != '.':
                 c = bfs(i,j,c)
 
-    if c == 0:
+    if c == 0: # bfs 탐색으로 4개 이상의 뿌요가 모인 경우를 찾지 못한 경우는 'answer'을 출력 한다. 
         print(answer)
         break
-    else:
-        answer = answer + 1
+    else: 
+        answer = answer + 1 # 4개 이상 찾은 경우는 answer + 1 한다.
     
-    fall()
+    fall() # fall 함수 호출.
--- BoJ/BoJ.13460.py
+++ BoJ/BoJ.13460.py
@@ -1,10 +1,67 @@
-import sys
+# input 받기
+from sys import stdin
 from collections import deque
+input = stdin.readline
 n,m = map(int,input().split())
-space = []
+a = []
 for i in range(n):
     s = list(input())
-    space.append(s)
+    a.append(s)
 
-print(space)
+check = [[[[False]*m for _ in range(n)] for _ in range(m)] for _ in range(n)] # 방문 여부를 확인할 check 배열을 4차원 배열로 선언한다. 
+dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1) # 배열의 인덱스는 [빨간 X좌표] [빨간 Y좌표] [파란 X좌표] [파란 Y좌표] 이다.
+q = deque()
 
+_rx, _ry, _bx, _by = [0]*4
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 'R':
+            _rx, _ry = i, j
+        elif a[i][j] == 'B':
+            _bx, _by = i, j
+q.append((_rx, _ry, _bx, _by, 0)) # Queue에 빨간 구슬의 (X, Y) 좌표, 파란 구슬의 (X, Y) 좌표를 모두 넣는다.
+check[_rx][_ry][_bx][_by] = True
+
+
+# 구슬의 다음 위치가 벽이라면 앞으로 가지 못한다. 구슬의 현재 위치가 구멍이라면, 현재 구슬의 색이 무엇인지 판별한다.
+def move(_x, _y, _dx, _dy, _c):
+    while a[_x+_dx][_y+_dy] != '#' and a[_x][_y] != 'O': # 구슬을 굴릴 때, 구슬의 다음 위치가 벽(#)인지, 구슬의 현재 위치가 구멍(O)인지 확인한다.
+        _x += _dx
+        _y += _dy
+        _c += 1
+    return _x, _y, _c
+
+def bfs():
+    while q:
+        rx, ry, bx, by, d = q.popleft()
+        if d >= 10: # 굴리는 과정이 10번을 넘어가면 그대로 종료하고, -1을 출력한다.
+            break
+
+        for i in range(4):
+            # 구슬을 굴리면서, 빨간 구슬의 이동 거리와 파란 구슬의 이동 거리를 카운트해야 한다.
+            nrx, nry, rc = move(rx, ry, dx[i], dy[i], 0)
+            nbx, nby, bc = move(bx, by, dx[i], dy[i], 0)
+
+            # 구슬의 다음 위치가 벽이라면 앞으로 가지 못한다. 구슬의 현재 위치가 구멍이라면, 현재 구슬의 색이 무엇인지 판별한다.
+            if a[nbx][nby] == 'O': # 만약 파란 구슬의 현재 위치가 구멍이라면 무시하고, 빨간 구슬의 현재 위치가 구멍이라면, d+1을 출력하고 종료한다.
+                continue
+            if a[nrx][nry] == 'O': 
+                print(d+1)
+                return
+
+            # 구슬을 굴린 후, 빨간 구슬의 위치와 파란 구슬의 위치가 같다면, 이동 거리 비교를 통해 겹치지 않도록 처리해야 한다.
+            if nrx == nbx and nry == nby: # 만약 구슬이 겹쳤다면, 굴릴 때 카운트했던 이동 거리가 더 긴 구슬의 위치를 한 칸 이전으로 되돌린다.
+                if rc > bc:
+                    nrx, nry = nrx-dx[i], nry-dy[i]
+                else:
+                    nbx, nby = nbx-dx[i], nby-dy[i]
+
+                    
+            if not check[nrx][nry][nbx][nby]:
+                check[nrx][nry][nbx][nby] = True
+                q.append((nrx, nry, nbx, nby, d+1)) 
+
+    print(-1) # 굴리는 과정이 10번을 넘어가면 그대로 종료하고, -1을 출력한다. // # 더 이상 갈 곳이 없을 때에는 BFS를 빠져나와서 -1을 출력한다.
+
+
+bfs()
