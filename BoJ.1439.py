add13887ce19f91d1820d03cffa902bed280e71b
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-11-18 10:37:26 +0900

    201118

---
+++ BoJ.1439.py
@@ -0,0 +1,21 @@
+l1 = list(input())
+r = []
+for i in range(len(l1)-1):
+
+    if l1[i] != l1[i+1]: # 현재와 그 뒤의 l1값이 다르면, 현재 l1[i]값 r에 추가
+        r.append(l1[i])
+
+r.append(l1[-1]) # 마지막 원소 추가
+
+
+c0 = 0 # r리스트에서 '0'의 갯수 count
+c1 = 0 # r리스트에서 '1'의 갯수 count
+for i in r:
+    if i == '0':
+        c0 = c0 + 1
+    elif i == '1':
+        c1 = c1 + 1
+
+# 0의 갯수와 1의 갯수중 작은값 print
+print(min(c0,c1))
+        
