aa825e6b2a52ce6fd6e24768dc58a76f055793ca
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-01-28 18:09:13 +0900

    210128

--- BoJ/BoJ.12865.py
+++ BoJ/BoJ.12865.py
@@ -11,7 +11,6 @@ for i in range(N):
 dp = [[0] * (K + 1) for i in range(N + 1)] # dp[N][K]
 
 for i in range(1,N+1):
-    
     for j in range(1,K+1):
         if j >= wlist[i]:
             dp[i][j] = max( dp[i-1][j], dp[i-1][ j-wlist[i] ] + vlist[i] )
