eac524a767b3dabd8317a43f0da84dd99db9f0aa
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-03-12 17:35:02 +0900

    210312

---
+++ BoJ/BoJ.17144.py
@@ -0,0 +1,30 @@
+R, C, T = map(int,input().split())
+space = []
+cleaner = []
+for i in range(R):
+    l = list(map(int,input().split()))
+    space.append(l)
+    if l[0] == -1:
+        cleaner.append(i)
+
+print(space)
+
+print(cleaner)
+
+
+time = 0
+while(time < T):
+
+    for i in range():
+        for j in range():
+    
+
+    ##
+
+
+    ##
+    
+
+
+
+    time = time + 1
