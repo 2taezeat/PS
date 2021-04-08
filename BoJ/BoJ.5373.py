afe2ab00509021e1d8f38aa577cf4c8db97ff5dc
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-04-08 19:54:35 +0900

    210408

---
+++ BoJ/BoJ.5373.py
@@ -0,0 +1,9 @@
+TC = int(input())
+for t in range(1,TC+1):
+    n = int(input())
+    qkdqjq = list(input())
+    for l in range(0,len(qkdqjq),+3):
+        side = qkdqjq[l:l+2][0]
+        dire = qkdqjq[l:l+2][1]
+
+        
