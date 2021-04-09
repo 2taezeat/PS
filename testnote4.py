2d96b3626e8ce6ab68a16af78f13373ebbbe3a3d
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-04-09 17:00:07 +0900

    210409

--- testnote4.py
+++ testnote4.py
@@ -1,102 +1,80 @@
-import sys
-input = sys.stdin.readline
+import itertools, copy, collections
+N, M = map(int,input().split())
+space = []
+result = []
+dy = [0,0,1,-1]
+dx = [-1,1,0,0]
+bias_loaction = []
+blankcount = 0
+for i in range(N):
+    aa = list(map(int,input().split()))
+    for a in range(len(aa)):
+        if aa[a] == 2:
+            bias_loaction.append((i,a))
+        elif aa[a] == 0:
+            blankcount +=1
 
-def cube(side, dir):
-    global u, d, f, b, l, r
-    if side == 'U' or side == 'D':
-        if side == 'U':
-            u = turn(u, dir)
-            i, j = 2, 0
-        else:
-            d = turn(d, dir)
-            i, j = 0, 2
-        temp_l = [list(x) for x in zip(*l)]
-        temp_r = [list(x) for x in zip(*r)]
-        temp = b[i]
-        if (side == 'U' and dir == '+') or (side == 'D' and dir == '-'):
-            b[i] = list(reversed(temp_l[i]))
-            temp_l[i] = f[j]
-            f[j] = list(reversed(temp_r[j]))
-            temp_r[j] = temp
-        else:
-            b[i] = temp_r[j]
-            temp_r[j] = list(reversed(f[j]))
-            f[j] = temp_l[i]
-            temp_l[i] = list(reversed(temp))
-        l = [list(x) for x in zip(*temp_l)]
-        r = [list(x) for x in zip(*temp_r)]
+    space.append(aa)
 
-    elif side == 'F' or side == 'B':
-        if side == 'F':
-            f = turn(f, dir)
-            i = 2
-        else:
-            b = turn(b, dir)
-            i = 0
-        temp = u[i]
-        if (side == 'F' and dir == '+') or (side == 'B' and dir == '-'):
-            u[i] = l[i]
-            l[i] = d[i]
-            d[i] = r[i]
-            r[i] = temp
-        else:
-            u[i] = r[i]
-            r[i] = d[i]
-            d[i] = l[i]
-            l[i] = temp
+comb = list ( itertools.combinations(bias_loaction,M) )
+def finish_check(n_space):
+    for i in range(N):
+        for j in range(N):
+            if n_space[i][j] == 0:
+                return False
+    return True
 
-    elif side == 'L' or side == 'R':
-        if side == 'L':
-            l = turn(l, dir)
-            i, j = 0, 2
-        else:
-            r = turn(r, dir)
-            i, j = 2, 0
-        temp_u = [list(x) for x in zip(*u)]
-        temp_d = [list(x) for x in zip(*d)]
-        temp_f = [list(x) for x in zip(*f)]
-        temp_b = [list(x) for x in zip(*b)]
-        temp = temp_b[i]
-        if (side == 'L' and dir == '+') or (side == 'R' and dir == '-'):
-            temp_b[i] = list(reversed(temp_d[j]))
-            temp_d[j] = list(reversed(temp_f[i]))
-            temp_f[i] = temp_u[i]
-            temp_u[i] = temp
-        else:
-            temp_b[i] = temp_u[i]
-            temp_u[i] = temp_f[i]
-            temp_f[i] = list(reversed(temp_d[j]))
-            temp_d[j] = list(reversed(temp))
-        u = [list(x) for x in zip(*temp_u)]
-        d = [list(x) for x in zip(*temp_d)]
-        f = [list(x) for x in zip(*temp_f)]
-        b = [list(x) for x in zip(*temp_b)]
+if blankcount == 0:
+    print(0)
+    exit()
+
+for cc in comb:
+    n_space = copy.deepcopy(space)
+    q = collections.deque()
+    for (i,j) in cc:
+        n_space[i][j] = 1000 # 활성 
+        q.append((i,j))
+    
+    bc = 0
+    # bfs
+    while (q):
+        #print(q)
+        # if finish_check(n_space) == True:
+        #     result.append(max(map(max, n_space)))
+        #     break
+    
+        y,x  = q.popleft()
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+            if 0<= ny <N and 0<= nx <N and n_space[ny][nx] != 1 and ( n_space[ny][nx] == 0 or n_space[ny][nx] == 2 ):
+                if n_space[ny][nx] == 2:
+                    n_space[ny][nx] = n_space[y][x] + 1
+                    q.append((ny,nx))
+                else:
+                    n_space[ny][nx] = n_space[y][x] + 1
+                    q.append((ny,nx))
+                    bc +=1
+
+        if bc == blankcount:
+            result.append(max(map(max, n_space)))
+            break
 
 
-def turn(side, dir):
-    if dir == '+':
-        return [list(reversed(x)) for x in zip(*side)]
-    else:
-        side = [list(x) for x in zip(*side)]
-        temp = side[0]
-        side[0] = side[2]
-        side[2] = temp
-        return side
+    # for i in range(N):
+    #     print(n_space[i])
+    
+    # print('------------')
+
+    # print(bc)
+                
+    
+    #result.append(max(map(max, n_space)))
+    # if finish_check(n_space) == True:
+    #     result.append(max(map(max, n_space)))
 
 
-tc = int(input())
-for _ in range(tc):
-    u = [['w' for _ in range(3)] for _ in range(3)]
-    d = [['y' for _ in range(3)] for _ in range(3)]
-    f = [['r' for _ in range(3)] for _ in range(3)]
-    b = [['o' for _ in range(3)] for _ in range(3)]
-    l = [['g' for _ in range(3)] for _ in range(3)]
-    r = [['b' for _ in range(3)] for _ in range(3)]
-
-    n = int(input())
-    case = list(input().strip().split())
-    for k in case:
-        side, dir = list(k)
-        cube(side, dir)
-    for s in u:
-        print(''.join(s))
+if result == []:
+    print(-1)
+else:
+    print(min(result) - 1000 )
--- testnote5.py
+++ testnote5.py
@@ -1,5 +1,7 @@
-arr = [[0,1,2],[3,4,5],[6,7,8]]
-result = list(zip(*arr[::-1])) # 우측 90도
-result2 = list(zip(*arr))[::-1] # 좌측 90도
-print(result)
-print(result2)
+arr = [[-1,1,100],[6,7,800],[7,4,500]]
+# result = list(zip(*arr[::-1])) # 우측 90도
+# result2 = list(zip(*arr))[::-1] # 좌측 90도
+# print(result)
+# print(result2)
+
+print(max(arr))
