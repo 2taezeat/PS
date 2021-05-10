0f9bdc0f3bf59189f38bd3fa5175910a47fb0a26
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-05-10 09:17:54 +0900

    210510

--- testnote2.py
+++ testnote2.py
@@ -1,65 +1,51 @@
-from itertools import permutations, combinations, product
-import copy
-def solution(n, weak, dist):
-    dist.sort(reverse=True)
-    #list1 = [i for i in range(1,n)] + [j for j in range(n,2*n)] # 원형 -> 2배 1차원 배열
-    #weak = set(weak)
-    for d in range(1,len(dist)+1):
-        friend = dist[0:d]
-        for p in permutations(weak,d):
-            tmp_weak = copy.deepcopy(weak)
-            set1 = set()
-            qjadnp = []
-            for i in range(len(p)):
-                min1 = p[i]
-                max1 = p[i] + friend[i]
-            
-                if max1 >= n:
-                    qjadnp.append((min1,n-1))
-                    qjadnp.append((0,max1 % n))
+from collections import deque
+import heapq
+def solution(n, k, cmd):
+    delete_list = []
+    q = [] ##
+    K = k
+    for i in range(n):
+        q.append(i)
 
-                else:
-                    qjadnp.append((min1,max1))
+    for i in range(len(cmd)):
+        if cmd[i][0] == 'D':
+            #a = cmd[i][2:]
+            K = K + int(cmd[i][2:])
 
+        elif cmd[i][0] == 'U':
+            K = K - int(cmd[i][2:])
 
-            for m1, m2 in qjadnp:
-                w = 0
-                while(w < len(weak)):
-                    if m1 <= tmp_weak[w] <= m2:
-                        set1.add(tmp_weak[w])
-#                        count += 1
-#                        tmp_weak.remove(w)
-                    w += 1
+        elif cmd[i][0] == 'C': #선택된행 삭제
+            if K == len(q) - 1 :
+                delete_list.append(q[K])
+                q.pop(K)
+                K = K - 1 
 
-                if len(set1) == len(weak):
-                    return d
+            else:
+                delete_list.append(q[K])
+                q.pop(K)
+                
+        elif cmd[i][0] == 'Z': #복구
+            back = delete_list.pop()
+            q.append(back)
+            q.sort()
+            idx = q.index(back)
+            if K >= idx:
+                K = K + 1
 
+        #print(q,delete_list,k,cmd[i])
 
+    answer = ''
 
-            # count = 0
-            # for i in range(len(p)):
-            #     for j in range(friend[i]+1):
-            #         k = (p[i] + j)
+    for i in range(n):
+        if i in q:
+            answer += 'O'
+        else:
+            answer += 'X'
 
-            #         if k > (2*n):
-            #             break
-                    
-            #         if k % n in tmp_weak:
-            #             tmp_weak.remove(k % n)
-                    
-            #         if len(tmp_weak) == 0:
-            #             return d
+    return answer
 
-            #         # try:
-            #         #     tmp_weak.remove( k % n )
-            #         # except:
-            #         #     pass
+#print(solution(8,2,["D 2","C","C","C","C","Z",'Z',"D 2","Z"]))
 
-            #         # if tmp_weak == []:
-            #         #     return d
-
-    return -1
-
-
-print(solution (12, [1, 5, 6, 10], [1, 2, 3, 4])  )
-#print(solution (12, [1, 3, 4, 9, 10], [3, 5, 7])  )
+print(solution(8,3,["D 2","C","U 3","C","D 3","C","U 2","Z","Z","U 1","C","U 1","C","Z"]))
+#print(solution(8,2,["D 2","C","C","C","C","Z",'Z',"D 2"]))
