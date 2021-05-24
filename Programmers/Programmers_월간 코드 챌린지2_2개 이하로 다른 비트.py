5f03f01b877a510590d9b9937069dd22132208a7
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-05-24 20:30:04 +0900

    210524_2개 이하로 다른 비트

--- Programmers/Programmers_월간 코드 챌린지2_2개 이하로 다른 비트.py
+++ Programmers/Programmers_월간 코드 챌린지2_2개 이하로 다른 비트.py
@@ -1,3 +1,20 @@
 def solution(numbers):
     answer = []
+    for n in numbers:
+        if n % 2 == 0:
+            answer.append(n+1)
+            continue
+        else:
+            b = bin(n)
+            b = '0' + b[2:]
+            b = list(b)
+            for i in range(len(b)-1,-1,-1):
+                if b[i] == '0':
+                    b[i] = '1'
+                    break
+            b[i+1] = '0'
+            answer.append( int(''.join(b),2) )
+
     return answer
+
+print(solution([3,7]))
