5a90ae7f5dedc612054610c7bc6cf85bb865dd0a
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-05-24 20:54:51 +0900

    210524_삼각 달팽이

--- Programmers/Programmers_월간 코드 챌린지1_삼각 달팽이.py
+++ Programmers/Programmers_월간 코드 챌린지1_삼각 달팽이.py
@@ -1,3 +1,31 @@
 def solution(n):
-    answer = []
-    return answer
+    answer = [[0 for j in range(1, i+1)] for i in range(1, n+1)] # 삼각형 구조 만들기
+    x, y = 0, -1 # 좌표값
+    num = 1 # 대입 숫자
+    for i in range(n): 
+        for j in range(i,n): 
+            if i % 3 == 0:
+                x = x
+                y = y + 1
+
+            elif i % 3 == 1:
+                x = x + 1
+                y = y
+            
+            elif i % 3 == 2:
+                x = x - 1
+                y = y - 1
+
+            answer[y][x] = num
+            num += 1
+
+    result = []
+    for a in answer:
+        for b in a:
+            result.append(b)
+
+    return result
+    print(result)
+
+
+print(solution(4))
