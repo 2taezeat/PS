a245a256bf7e412604f445e5719e4d24f32cc621
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-05-27 21:14:40 +0900

    210527_전화번호 목록

---
+++ Programmers/Programmers_코딩테스트 연습_해시_전화번호 탐색.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+def solution(phone_book):
+    phone_book.sort()
+
+
+    for i in range(len(phone_book)-1):
+        l = len(phone_book[i])
+        sta = phone_book[i]
+
+        if sta == phone_book[i+1][:l]:
+            return False
+
+
+    return True
+
+print(solution(["119", "97674223", "1195524421"]))
