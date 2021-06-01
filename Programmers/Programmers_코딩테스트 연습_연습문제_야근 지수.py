e4e8b77db24f37a8fbcfae66e12c2e7ccaff4532
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-01 19:12:47 +0900

    210601_야근 지수

--- Programmers/Programmers_코딩테스트 연습_연습문제_야근 지수.py
+++ Programmers/Programmers_코딩테스트 연습_연습문제_야근 지수.py
@@ -1,3 +1,19 @@
+import heapq
 def solution(n, works):
     answer = 0
+    h = []
+    for w in works:
+        heapq.heappush(h,[-w,w])
+
+    for i in range(n):
+        heapq.heapify(h)
+        if h[0][1] != 0 :
+            h[0][1] -= 1
+            h[0][0] += 1
+
+    for (a,b) in h:
+        answer += b**2
+
     return answer
+
+print(solution( 4, [4,3,3] ))
