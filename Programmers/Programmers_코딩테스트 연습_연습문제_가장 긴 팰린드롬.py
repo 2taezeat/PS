19582d3c0dc41587d7d403fa306fda3526f39936
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-15 19:16:06 +0900

    210615_가장 긴 팰린드롬

--- Programmers/Programmers_코딩테스트 연습_연습문제_가장 긴 팰린드롬.py
+++ Programmers/Programmers_코딩테스트 연습_연습문제_가장 긴 팰린드롬.py
@@ -1,4 +1,22 @@
-def solution(s):
-    answer = 0
+def check(word):
+    for i in range(len(word) // 2):      # 0부터 문자열 길이의 절반만큼 반복
+        if word[i] != word[-1 - i]:      # 왼쪽 문자와 오른쪽 문자를 비교하여 문자가 다르면
+            return False
 
-    return answer
+    return True
+
+def solution(s):
+    for l in range(len(s),0,-1):
+        i = 0
+        while True:
+            if i + l > len(s):
+                break
+
+            word = s[i:i+l]
+            # print(word)
+            if check(word):
+                return l
+
+            i += 1
+
+    return 0
