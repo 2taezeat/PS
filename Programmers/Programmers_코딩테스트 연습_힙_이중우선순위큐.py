b10553a2c25225e8a4e86913887e88ebaaff20eb
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-16 15:28:13 +0900

    210616

--- Programmers/Programmers_코딩테스트 연습_힙_이중우선순위큐.py
+++ Programmers/Programmers_코딩테스트 연습_힙_이중우선순위큐.py
@@ -1,3 +1,26 @@
+import heapq
 def solution(operations):
-    answer = []
-    return answer
+    h1 = [] ## 최대 힙
+    h2 = [] ## 최소 힙
+    for o in operations:
+        if o[0] == 'I':
+            item = int(o[2:])
+            heapq.heappush(h1,(-item, item))
+            heapq.heappush(h2,(item, item))
+
+        elif o[-1] == '1' and o[-2] == ' ':
+            if h1 == []:
+                continue
+            v = heapq.heappop(h1)[1]
+            h2.remove((v,v))
+
+        elif o[-2] == '-':
+            if h1 == []:
+                continue
+            v = heapq.heappop(h2)[1]
+            h1.remove((-v,v))
+
+    if h1 == []:
+        return [0,0]
+
+    return [h1[0][1],h2[0][1]]
