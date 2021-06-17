4104f1ef9d52e4b9468fe9802edc22a267f3eca5
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-17 17:07:32 +0900

    210617_숫자게임

--- Programmers/Programmers_SW Coding(2018)_숫자 게임.py
+++ Programmers/Programmers_SW Coding(2018)_숫자 게임.py
@@ -1,3 +1,20 @@
 def solution(A, B):
-    answer = -1
+    answer = 0
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+
+    for i in range(len(A)):
+        flag = False
+        sta = A[i]
+        for j in range(len(B)):
+            if sta < B[j]:
+                sta = B[j]
+                flag = True
+            else:
+                break
+
+        if flag == True:
+            answer += 1
+            B.remove(sta)
+
     return answer
