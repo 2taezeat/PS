4c9c748ee74132f210f77660a0a4dd5a9bda3aea
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-26 12:30:57 +0900

    210626

--- testnote1.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = [0,1,2]
-    if n == 1 or n == 2:
-        return answer[n]
-    
-    for i in range(3,n+1):
-        answer.append(answer[i-1] + answer[i-2])
-        
-    return answer[-1] % 1234567
