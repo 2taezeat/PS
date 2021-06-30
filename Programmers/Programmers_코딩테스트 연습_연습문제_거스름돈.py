16ba0aa975816bbc3b7b0b6aeb7934c6ab14fd4e
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-06-30 17:32:30 +0900

    210630

--- Programmers/Programmers_코딩테스트 연습_연습문제_거스름돈.py
+++ Programmers/Programmers_코딩테스트 연습_연습문제_거스름돈.py
@@ -1,3 +1,10 @@
 def solution(n, money):
-    answer = 0
-    return answer
+    dp = [1] + [0]*n
+    for coin in money:
+        for price in range(coin,n+1):
+            dp[price] += dp[price - coin]
+            
+    return dp[n] % 1000000007
+
+
+print(solution(6,[1,2,5]))
